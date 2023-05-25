25bbc88857eaeb37074322a09a6f29930d7108e9
sophorntoeum <sophorn.toeum@student.passerellesnumeriques.org>
2023-05-25 17:40:12 +0700

    did PUT request and update in drone and add unique name in plan

--- app/Models/Drone.php
+++ app/Models/Drone.php
@@ -20,10 +20,10 @@ class Drone extends Model
         'indruction_id'
     ];
 
-    public static function drone($request, $id=null)
+    public static function drone($request, $drone_id=null)
     {
         $drone = $request->only(['drone_id', 'drone_name', 'drone_type', 'sensor', 'playoad_capacity', 'batter_life','user_id', 'indruction_id']);
-        $drone = self::updateOrCreate(['id'=>$id], $drone);
+        $drone = self::updateOrCreate(['drone_id'=>$drone_id], $drone);
         return $drone;
     }
     public function user()
